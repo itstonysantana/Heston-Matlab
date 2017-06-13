6120fef21ed54c0777309e92b1e7432bf1a94d99
itstonysantana <hanthonysabree@yahoo.com>
2017-06-13 07:52:49 -0400

    optimized code
    commented out for loop, preallocated array

--- StochJumpPath.m
+++ StochJumpPath.m
@@ -6,13 +6,21 @@ RandMat2=rand(Rep,NSteps);
 RandMat1=norminv(RandMat1);
 RandMat2=norminv(RandMat2);
 RandMat3=rho*RandMat1+sqrt(1-rho*rho)*RandMat2;
-            for i=1:Rep
-      for j=2:(NSteps+1)
-            V(i,1)=Vo;
-            SPP(i,1)=so;
-      end
-    end
-    for i=1:Rep %Euler schemes with jump processes
+% Below is the old code for creating the zero matrices and populating the
+% first column of each with the initial stock price and the initial
+% variance, respectively:
+%
+%     for i=1:Rep
+%       for j=2:(NSteps+1)
+%             V(i,1)=Vo;
+%             SPP(i,1)=so;
+%       end
+%     end
+SPP = zeros(Rep,NSteps+1); % this code preallocates the memory, then does the above
+V = zeros(Rep,NSteps+1);   %
+SPP(:,1) = so;             %
+V(:,1) = Vo;               %
+    for i=1:Rep % Euler schemes with jump processes
         for j=2:(NSteps+1)
             J=0;
             if lam_j ~= 0
@@ -23,11 +31,10 @@ RandMat3=rho*RandMat1+sqrt(1-rho*rho)*RandMat2;
                     end
                 end
             end
-    V(i,j)=V(i,(j-1))+k1*(phi1-V(i,(j-1)))*dt+(sigma1*(sqrt(V(i,(j-1))))*sqrt(dt)*RandMat1(i,(j-1)));
-    V1=abs(V); %prevents negative volatilities
-    SPP(i,j)=SPP(i,(j-1))+((r+q).*SPP(i,(j-1))*dt+(sqrt(V1(i,(j-1))))*SPP(i,(j-1))*sqrt(dt)*(RandMat3(i,(j-1))))+J;
-            j=j+1;
-        end
+        V(i,j)=V(i,(j-1))+k1*(phi1-V(i,(j-1)))*dt+(sigma1*(sqrt(V(i,(j-1))))*sqrt(dt)*RandMat1(i,(j-1)));
+        V1=abs(V); %prevents negative volatilities
+        SPP(i,j)=SPP(i,(j-1))+((r+q).*SPP(i,(j-1))*dt+(sqrt(V1(i,(j-1))))*SPP(i,(j-1))*sqrt(dt)*(RandMat3(i,(j-1))))+J;
+         end
     end
 SPaths=SPP;
 Vt=V1;
