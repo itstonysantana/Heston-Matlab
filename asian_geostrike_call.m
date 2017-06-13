05ebe7f93f77c187d2bae4cc0bfef089c0feb6d8
itstonysantana <hanthonysabree@yahoo.com>
2017-06-13 07:57:56 -0400

    changed output

--- asian_geostrike_call.m
+++ asian_geostrike_call.m
@@ -1,4 +1,4 @@
-function [x] = asian_geostrike_call(so,K,r,R,q,rho,sigma, sigma1,k,phi,lam_j,mean_j,vol_j,T,NSteps,Rep)
+function [option_value] = asian_geostrike_call(so,r,R,q,rho,sigma, sigma1,k,phi,lam_j,mean_j,vol_j,T,NSteps,Rep)
 x = StochJumpPath(so,r,q,rho,sigma, sigma1,k,phi,lam_j,mean_j,vol_j,T,NSteps,Rep);
 x = x(:,2:NSteps);
 geo_mean = geomean(x,2);
@@ -13,4 +13,3 @@ for elm = geo_mean
     index = index + 1;
 end
 option_value = mean(m)*exp(-R*T);
-disp(option_value)
